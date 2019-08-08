5e93b987aa6d68d66296c6ffa766368715eb26e5
lpierrot <pierrotlione@seattleu.edu>
2019-08-07 21:57:18 -0700

    made changes to the projects to use boostrap-vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,93 @@
 <template>
-  <div class="Home">
-    <h2>Civic App</h2>
-    <p>
-    <router-link to="/results">Learn more about cancidates and upcoming elections.</router-link>
-    </p>
-    <form v-on:submit.prevent="findResults">
-      <p>Enter your address: <input type="text" v-model="elections"> <button type="submit">Search</button></p>
-    </form>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Search:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.search"
+          :options="search"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-4">
+        <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
+          <b-form-checkbox value="me">Check me out</b-form-checkbox>
+          <b-form-checkbox value="that">Check that out</b-form-checkbox>
+        </b-form-checkbox-group>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Home',
-  props: {
-    msg: String
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          name: '',
+          search: null,
+          checked: []
+        },
+        searchs: [{ text: 'Select One', value: null }, 'Elections', 'Officals', 'Levels'],
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.form. search = null
+        this.form.checked = []
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-body {
+<style>
+.home {
   background-color: #f0e9e0;
 }
 </style>
